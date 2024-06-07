72d0f92f0ed1dd11ec945b841baf89a1b89c6d61
Franc <Franc@DESKTOP-DRT12IO>
2024-06-07 19:03:21 +0200

    Add project files.

---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,82 @@
+﻿using Ispit.Model;
+using System;
+
+namespace Ispit.Konzola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PokreniMetode();
+
+            Console.ReadKey();  
+        }
+
+        static void PokreniMetode()
+        {
+            UnesiPodatkeUcenika();           
+        }
+
+        static void UnesiPodatkeUcenika()
+        {
+            Ucenik[] ucenici = new Ucenik[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                ucenici[i] = new Ucenik();
+
+                Console.WriteLine($"Upišite ime {i + 1}. učenika: ");
+                ucenici[i].Ime = Console.ReadLine();
+
+                Console.WriteLine($"Upišite prezime {i + 1}. učenika: ");
+                ucenici[i].Prezime = Console.ReadLine();
+
+                bool isValid = false;
+                while (!isValid)
+                {
+                    Console.Write($"Upišite datum rođenja {i + 1}. učenika(DD.MM.YYYY): ");
+                    isValid = DateTime.TryParse(Console.ReadLine(), out DateTime datumRodjenja);
+                    if (isValid)
+                    {
+                        ucenici[i].DatumRodjenja = datumRodjenja;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Niste upisali ispravan datum rođenja učenika!");
+                    }
+                }
+
+                isValid = false;
+                while (!isValid)
+                {
+                    Console.Write($"Upišite prosjek {i + 1}. učenika (1-5): ");
+                    isValid = Double.TryParse(Console.ReadLine(), out double prosjek);
+                    if (isValid)
+                    {
+                        ucenici[i].Prosjek = prosjek;
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Niste upisali ispravan prosjek");
+                }
+            }
+
+            IspisiPodatkeUcenika(ucenici);
+
+        }
+
+        static void IspisiPodatkeUcenika(Ucenik[] ucenici)
+        {
+            foreach (var ucenik in ucenici)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Ime: {ucenik.Ime}");
+                Console.WriteLine($"Prezime: {ucenik.Prezime}");
+                Console.WriteLine($"Datum rođenja: {ucenik.DatumRodjenja.ToShortDateString()}");
+                Console.WriteLine($"Starost: {ucenik.VratiStarost()} godina");
+                Console.WriteLine($"Prosjek: {ucenik.Prosjek} ({ucenik.ProsjekRijecima(ucenik.Prosjek)})");
+            }
+        }
+
+    }
+}
---
+++ Ispit.Model/Ucenik.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ispit.Model
+{
+    public class Ucenik
+    {
+        private string _ime;
+        private string _prezime;
+        int _starost;
+
+        public string Ime { get; set; }
+
+        public string Prezime { get; set; }
+       
+        public DateTime DatumRodjenja { get; set; }
+
+        public double Prosjek { get; set; }
+
+        public string VratiPunoIme()
+        {
+            return $"{_ime} {_prezime}";
+        }
+
+        public string ProsjekRijecima(double prosjek)
+        {
+            if (prosjek >= 1.0 && prosjek <= 1.49)
+                return "nedovoljan";
+            else if (prosjek >= 1.5 && prosjek <= 2.49)
+                return "dovoljan";
+            else if (prosjek >= 2.5 && prosjek <= 3.49)
+                return "dobar";
+            else if (prosjek >= 3.5 && prosjek <= 4.49)
+                return "vrlo dobar";
+            else if (prosjek >= 4.5 && prosjek <= 5.0)
+                return "odličan";
+            else
+                return "Niste upisali ispravan prosjek, prosjek treba biti broj između 1 i 5";
+        }
+
+        public int VratiStarost()
+        {
+            DateTime trenutniDatum = DateTime.Now;
+            _starost = trenutniDatum.Year - DatumRodjenja.Year;
+            return _starost;
+        }
+    }
+}
